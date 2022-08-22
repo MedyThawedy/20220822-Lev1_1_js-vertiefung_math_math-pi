17196f3a195c2ef90ecdd3eedf08f913bcad5aec
MD <dhmedy@googlemail.com>
2022-08-22 15:48:58 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+
+// Zeige in der Konsole Math.PI.
+console.log(Math.PI);
+
+// Deklariere die const PI mit dem passenden Wert.
+const PI = Math.PI;
+console.log(PI);
+
+// Runde den Wert von PI auf zwei Dezimalstellen und weise den Wert der Variablen roundedPI zu.
+// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+roundedPI = PI.toFixed(2);
+
+
+// Gib roundedPI in der Konsole aus.
+console.log(roundedPI);
